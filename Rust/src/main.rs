a409a1ac74592d5121f55a05736c421801647f84
Mezzano <ricoribeiroreis@gmail.com>
2026-03-04 16:23:51 -0300

    "Hello world"
    did not commit any of the debug fiiles since there is to many and its no necessary

---
+++ Rust/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    //  vvv "mut" makes the variable mutable, allowing us to change its value later if needed.
+    let mut name: &str = "Richas";
+    let age: i32 = 20;
+    println!("Hello, {}!", name);
+    println!("You are {} years old.", age);
+}
